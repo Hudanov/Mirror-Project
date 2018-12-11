d5d2bc1a73a3b0251267dabfc6462d4f526a1098
hudanov <scalesymmetry@gmail.com>
2018-12-11 14:00:57 +0200

    weather script

--- Page 1/weather.js
+++ Page 1/weather.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-  getURL();  
+  getURL(); 
+  getIcon();
 });
 
 
@@ -32,10 +33,10 @@ function getJson(url) {
       url: url,
       success: function(json) {
         var cel = fahrenheit_to_сelsius(json.currently.temperature);
-        $("#weather-current").html("Current temperature: " + (cel.toFixed(1))+" °C");
+        $("#weather-current").html("Current temperature: " + (cel.toFixed(1))+"° ");
         $("#weather-high").html("The highest day temperature: "+fahrenheit_to_сelsius(Math.round(json.daily.data[0].temperatureMax)).toFixed(1)+"°");
         $("#weather-low").html("The lowest day temperature: "+fahrenheit_to_сelsius(Math.round(json.daily.data[0].temperatureMin)).toFixed(1)+"°");
-        //setBackground(json.currently.icon);
+        //getIcon(json.currently.icon);
       }
 
     })
@@ -44,4 +45,13 @@ function getJson(url) {
 
 function fahrenheit_to_сelsius(temperature_in_fahrenheit) {
     return (temperature_in_fahrenheit - 32) * 5 / 9;
+}
+
+function getIcon()
+{
+  //var iconDescription = icon.toUpperCase().replace( /-/g, "_");
+  //alert(iconDescription) ;
+  var icons = new Skycons({"color": "white"});
+  icons.set("a", Skycons.SNOW);
+  icons.play();
 }
